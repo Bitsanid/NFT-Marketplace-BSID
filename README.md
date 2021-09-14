e57b792da117743620a3a7c4d6e0e5942e011f87
abraham-yusuf <1703020028@students.unis.ac.id>
2021-09-14 12:21:49 +0000

    already run in testnet mumbai polygon

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # abraham-yusuf-dapp
 My First Decentrallized Applications
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/deploy.js
+npx hardhat help
+```
---
+++ config.js
@@ -0,0 +1,5 @@
+export const nftmarketaddress = "0xa202CaE2b835155A5b188986EA82913e580a1Ec0"
+export const nftaddress = "0x8D61e61cC5eAD9202D342594f3Ae3a576BbC8701"
+
+//export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+//export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
